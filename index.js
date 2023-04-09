48e9b4a6f452f35b9dc0a6b06935406946361fb9
cus <erreapesrk@gmail.com>
2023-04-09 13:49:58 -0600

    integrando componentes con js en un archivo principal

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const navbarEmail = document.querySelector('.navbar-email');
-const desktopMenu = document.querySelector('.desktop-menu');
+const desktopMenu = document.querySelector('.desktop-menu');\
+const iconBurger = document.querySelector('')
 
 navbarEmail.addEventListener('click', toggleDesktopMenu);
 
